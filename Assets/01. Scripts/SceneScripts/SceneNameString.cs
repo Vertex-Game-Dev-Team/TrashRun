a52ab4962ced750a663861ef94e6c194dc122895
Mingyeol Kim <neomingyeol@gmail.com>
2024-10-14 00:20:11 +0900

    수정 | 무기 업그레이드 부분 기획 더 세세하게

--- Assets/01. Scripts/SceneScripts/SceneNameString.cs
+++ Assets/01. Scripts/SceneScripts/SceneNameString.cs
@@ -10,5 +10,5 @@ public class SceneNameString
     public const string _00_Logo = "00.Logo";
     public const string _01_Title = "01.Title";
     public const string _02_Main = "02.Main";
-    public const string _03_GamePlay = "03.GamePlay";
+    public const string _03_Game = "03.Game";
 }
